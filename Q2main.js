99c5dde53327ad18e3b694afa0608e6e6d2723b8
Hira Mohammad <161503313+Hira-Mohammad@users.noreply.github.com>
2024-03-10 15:44:11 +0500

    Add files via upload

---
+++ Q2main.js
@@ -0,0 +1,4 @@
+// this saves the name
+var personname = "hiramohammad";
+// this shows the message
+console.log('Hello {personname}, would you like to learn some typescript today?');
---
+++ Q2main.ts
@@ -0,0 +1,4 @@
+// this saves the name
+let personname: string = "hiramohammad"; 
+// this shows the message
+ console.log('Hello {personname}, would you like to learn some typescript today?');
